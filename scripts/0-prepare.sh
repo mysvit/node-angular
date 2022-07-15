e252bf94a82f099c7a16a59ca73e80b9fe67a409
Summer Mix <smix@tuta.io>
2022-07-15 14:44:23 +0300

    angular cli 14.0.6

--- scripts/0-prepare.sh
+++ scripts/0-prepare.sh
@@ -29,7 +29,7 @@ dockerImageBuild() {
   cd scripts/docker-images/devnode
 
   docker build --build-arg NPM_VERSION='latest' \
-               --build-arg ANGULAR_CLI_VERSION='14.0.3' \
+               --build-arg ANGULAR_CLI_VERSION='14.0.6' \
                --build-arg PWD=$(pwd) \
                -t devnode .
 
