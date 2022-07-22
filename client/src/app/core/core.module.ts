44ef2251153be0d1fa3ede3c79cd8a1248054ccc
Summer Mix <smix@tuta.io>
2022-07-22 14:23:08 +0300

    app style

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -1,14 +1,25 @@
 import { CommonModule } from '@angular/common'
 import { NgModule } from '@angular/core'
-import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component'
+import { MatButtonModule } from '@angular/material/button'
+import { MatIconModule } from '@angular/material/icon'
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component'
 
 
 @NgModule({
     declarations: [
-        NotFoundPageComponent
+        NotFoundPageComponent,
+        ToolbarComponent
+    ],
+    exports: [
+        ToolbarComponent
     ],
     imports: [
-        CommonModule
+        CommonModule,
+        MatToolbarModule,
+        MatIconModule,
+        MatButtonModule
     ]
 })
 export class CoreModule {
