2679debd4f6d3196678bcba13db79767002b211c
Summer Mix <smix@tuta.io>
2022-07-29 18:31:11 +0300

    clean

--- client/src/app/shared/components/fields/email-field/email-field.component.ts
+++ client/src/app/shared/components/fields/email-field/email-field.component.ts
@@ -9,27 +9,14 @@ import { AbstractValueAccessor, MakeProvider } from '../abstract-value-accessor'
 })
 export class EmailFieldComponent extends AbstractValueAccessor implements OnInit {
 
-    // formC: FormControl = new FormControl('')
-
     constructor() {
         super()
-        // console.debug('constructor', this.formC)
     }
 
-    // getErrorMessage() {
-    //     if (this.formC.hasError('required')) {
-    //         return 'You must enter an email'
-    //     }
-    //
-    //     return this.formC.hasError('email') ? 'Not a valid email' : ''
-    // }
-
     ngOnInit(): void {
-        // console.debug('ngOnInit', this.formC)
         setTimeout(() => {
             console.debug('ngOnInit.value', this, this.value)
         }, 2000)
-
     }
 
 }
--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -3,13 +3,9 @@ import { NgModule } from '@angular/core'
 import { ReactiveFormsModule } from '@angular/forms'
 import { MatInputModule } from '@angular/material/input'
 import { EmailFieldComponent } from './components/fields/email-field/email-field.component'
-import { PasswordFieldComponent } from './components/fields/password-field/password-field.component'
-import { UsernameFieldComponent } from './components/fields/username-field/username-field.component'
 
 const fields = [
-    EmailFieldComponent,
-    UsernameFieldComponent,
-    PasswordFieldComponent
+    EmailFieldComponent
 ]
 
 @NgModule({
