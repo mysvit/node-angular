83dc9979b28e70f7866b6b3204fb5c112149dcd8
Summer Mix <smix@tuta.io>
2022-09-06 11:18:01 +0300

    client test fix

--- client/src/app/shared/components/fields/email-field/email-field.component.spec.ts
+++ client/src/app/shared/components/fields/email-field/email-field.component.spec.ts
@@ -1,23 +1,22 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { EmailFieldComponent } from './email-field.component';
+import { ComponentFixture, TestBed } from '@angular/core/testing'
+import { EmailFieldComponent } from './email-field.component'
 
 describe('EmailFieldComponent', () => {
-  let component: EmailFieldComponent;
-  let fixture: ComponentFixture<EmailFieldComponent>;
+    let component: EmailFieldComponent
+    let fixture: ComponentFixture<EmailFieldComponent>
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ EmailFieldComponent ]
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [EmailFieldComponent]
+        })
+            .compileComponents()
+
+        fixture = TestBed.createComponent(EmailFieldComponent)
+        component = fixture.componentInstance
+        fixture.detectChanges()
     })
-    .compileComponents();
 
-    fixture = TestBed.createComponent(EmailFieldComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+    it('should create', () => {
+        expect(component).toBeTruthy()
+    })
+})
