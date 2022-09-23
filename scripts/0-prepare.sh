b505421dd3f1249763ae06738f1d83e0a9b635a8
Summer Mix <smix@tuta.io>
2022-09-23 13:06:33 +0300

    script update

--- scripts/0-prepare.sh
+++ scripts/0-prepare.sh
@@ -28,8 +28,10 @@ dockerImageBuild() {
 
   cd scripts/docker-images/devnode
 
-  docker build --build-arg NPM_VERSION='latest' \
-               --build-arg ANGULAR_CLI_VERSION='14.2.2' \
+  docker build \
+               --build-arg NODE_VERSION='16.17.0' \
+               --build-arg NPM_VERSION='latest' \
+               --build-arg ANGULAR_CLI_VERSION='14.2.3' \
                --build-arg PWD=$(pwd) \
                -t devnode .
 
